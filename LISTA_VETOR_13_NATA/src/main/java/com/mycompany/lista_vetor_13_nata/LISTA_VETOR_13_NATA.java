18ed78cd07aec356d378fc4a425ba16f4fe5e4bb
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-09-18 13:15:34 -0300

    Add files via upload

---
+++ LISTA_VETOR_13_NATA/src/main/java/com/mycompany/lista_vetor_13_nata/LISTA_VETOR_13_NATA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_vetor_13_nata;
+
+/**
+ *
+ * @author Nata e Julia
+ */
+public class LISTA_VETOR_13_NATA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
