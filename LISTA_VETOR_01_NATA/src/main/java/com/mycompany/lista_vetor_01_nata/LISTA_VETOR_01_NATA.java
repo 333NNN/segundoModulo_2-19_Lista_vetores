07b89f77e05d12e28709b257b98b57f4887d4abe
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-09-18 18:38:52 -0300

    Add files via upload

---
+++ LISTA_VETOR_01_NATA/src/main/java/com/mycompany/lista_vetor_01_nata/LISTA_VETOR_01_NATA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_vetor_01_nata;
+
+/**
+ *
+ * @author ncunha
+ */
+public class LISTA_VETOR_01_NATA {
+
+    public static void main(String[] args) {
+        int[] n = new int[51];
+
+        for (int i = 0; i < n.length; i++) {
+            n[i] = i;
+            System.out.print(n[i] + " ");
+        }
+    }
+}
